de003f208c31c9abbb4159358284384151b5440b
Linzp <sunandmoon001@163.com>
2026-07-01 16:58:07 +0800

    修正部分文件不能正确应用模版的bug

--- index.js
+++ index.js
@@ -4,21 +4,37 @@ const ejs = require('ejs');
 const path = require('path');
 const textExtensions = require('text-extensions');
 
+const EXTENSIONLESS_TEMPLATE_FILES = new Set([
+    'Dockerfile',
+    'dockerignore',
+    'gitignore',
+    '.dockerignore'
+]);
+
+const shouldRenderAsTemplate = (filePath) => {
+    const ext = path.extname(filePath).slice(1);
+    if (textExtensions.indexOf(ext) > -1) {
+        return true;
+    }
+    return EXTENSIONLESS_TEMPLATE_FILES.has(path.basename(filePath));
+};
+
 const apply = async (dir, to, options) => {
     dir = path.resolve(dir);
     to = path.resolve(to);
     await fs.ensureDir(to);
     const paths = await glob('**/*', {
-        cwd: dir, dot: true
+        cwd: dir, dot: true, nodir: true
     });
     for (let filePath of paths) {
+        const sourcePath = path.resolve(dir, filePath);
         const targetDir = path.resolve(to, filePath);
-        if (textExtensions.indexOf(path.extname(filePath).slice(1)) > -1) {
-            const file = await fs.readFile(path.resolve(dir, filePath), 'utf8');
+        if (shouldRenderAsTemplate(filePath)) {
+            const file = await fs.readFile(sourcePath, 'utf8');
             await fs.ensureDir(path.dirname(targetDir));
             await fs.writeFile(targetDir, ejs.render(file, options));
         } else {
-            await fs.copy(path.resolve(dir, filePath), targetDir);
+            await fs.copy(sourcePath, targetDir);
         }
     }
 };
